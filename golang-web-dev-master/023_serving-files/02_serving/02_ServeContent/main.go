ab150531d06ae6a7ca264f93b43459ff0aa7718f
NDUBUISI CHRISTOPHER OKPALA <60747096+NDCHRIS2003@users.noreply.github.com>
2020-10-22 16:36:44 -0600

    add to the repository

---
+++ golang-web-dev-master/023_serving-files/02_serving/02_ServeContent/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/toby.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/toby.jpg">`)
+}
+
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("toby.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+
+	http.ServeContent(w, req, f.Name(), fi.ModTime(), f)
+
+	// ServeContent ask for responseWriter, pointer to request, name of the file, the last time the file was modifiled and openfile(actual file)
+}
